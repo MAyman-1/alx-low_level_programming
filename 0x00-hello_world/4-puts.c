d14014e76b820932a99a55b4a242ac053b6f0a24
MAyman <medoyea@gmail.com>
2023-07-16 12:21:05 -0700

    fix typo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /* print with puts instead of printf */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle);
 	return (o);
 }
