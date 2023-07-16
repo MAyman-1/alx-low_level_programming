ac0419fb5a5edb8742bd9d5f9cb87aae10195744
MAyman <medoyea@gmail.com>
2023-07-16 12:29:51 -0700

    providing comment description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* print with puts instead of printf */
+/**
+ * main - printing with puts instead of printf
+ * return 0 if successful
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle);
